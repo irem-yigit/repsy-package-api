c6413d6930d16a9f6b04592684b1387ef6e30a2f
irem-yigit <yigitiremm@gmail.com>
2025-04-24 23:47:00 +0300

    fix: removed @Service annotations from FileSystemStorageService and ObjectStorageService to prevent ambiguity in StorageService injection.

--- file-system-storage/src/main/java/com/repsy/storage/filesystem/service/FileSystemStorageService.java
+++ file-system-storage/src/main/java/com/repsy/storage/filesystem/service/FileSystemStorageService.java
@@ -8,7 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Service
+
 public class FileSystemStorageService implements StorageService{
 
     private final Path baseDir;
--- object-storage/src/main/java/com/repsy/storage/object/service/ObjectStorageService.java
+++ object-storage/src/main/java/com/repsy/storage/object/service/ObjectStorageService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 
-@Service
+
 public class ObjectStorageService implements StorageService {
 
     private final MinioClient minioClient;
